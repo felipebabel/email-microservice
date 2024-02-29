981e80f35bf85e2db66bc26624431d4e8b28e820
Felipe Babel <Felipe.Babel@philips.com>
2024-02-29 20:59:48 -0300

    fix: fix service

--- email-microservice/src/main/java/com/emailmicroservice/application/EmailSenderService.java
+++ email-microservice/src/main/java/com/emailmicroservice/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.emailmicroservice.application;
 import com.emailmicroservice.adapters.EmailSenderGateway;
 import com.emailmicroservice.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
--- email-microservice/src/main/java/com/emailmicroservice/infrastructure/amazonses/AmazonSesEmailSender.java
+++ email-microservice/src/main/java/com/emailmicroservice/infrastructure/amazonses/AmazonSesEmailSender.java
@@ -1,5 +1,41 @@
 package com.emailmicroservice.infrastructure.amazonses;
 
-public class AmazonSesEmailSender {
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.emailmicroservice.adapters.EmailSenderGateway;
+import com.emailmicroservice.core.exception.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AmazonSesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public AmazonSesEmailSender(final AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void senderEmail(final String to,
+                            final String subject,
+                            final String body) {
+        final SendEmailRequest request = new SendEmailRequest();
+        request.withSource("babelfelipe@gmail.com")
+            .withDestination(new Destination().withToAddresses(to))
+            .withMessage(new Message().withBody(new Body().withText(new Content(body)))
+                .withSubject(new Content(subject)));
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException e) {
+            throw new EmailServiceException("Error when send email");
+        }
+    }
 
 }
