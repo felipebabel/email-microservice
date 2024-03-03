8d5045ad301b74210b2d4fd5654dcd04f30e4864
Felipe Babel <Felipe.Babel@philips.com>
2024-03-02 22:32:13 -0300

    feat: default response, dto doc, messages

--- src/main/java/com/emailmicroservice/core/constant/Messages.java
+++ src/main/java/com/emailmicroservice/core/constant/Messages.java
@@ -12,4 +12,8 @@ public class Messages {
 
     public static final String MSG_FIELD_ID_NOT_INFORMED = "%s not informed.";
 
+    public static final String MSG_EMAIL_SENT_SUCCESSFUL = "Email sent successful";
+
+    public static final String MSG_ERROR_WHEN_SEND_EMAIL = "Error when send email";
+
 }
--- src/main/java/com/emailmicroservice/core/dto/EmailDto.java
+++ src/main/java/com/emailmicroservice/core/dto/EmailDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.annotations.ApiModelProperty;
+
 
 @Builder
 @NoArgsConstructor
@@ -11,9 +13,16 @@ import lombok.NoArgsConstructor;
 @Data
 public class EmailDto {
 
+    @ApiModelProperty(value = "Recipient email address", example = "recipient@example.com")
     private String to;
+
+    @ApiModelProperty(value = "Email subject", example = "Hive a nice day!")
     private String subject;
+
+    @ApiModelProperty(value = "Email body", example = "Hello, what's your favorite food?")
     private String body;
+
+    @ApiModelProperty(value = "Sender email address", example = "sender@example.com")
     private String from;
 
 }
