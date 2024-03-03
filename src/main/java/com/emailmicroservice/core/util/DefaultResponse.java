e3508e762393404e46915b97385d421496a8cc95
Felipe Babel <Felipe.Babel@philips.com>
2024-03-03 16:30:48 -0300

    fix: default response

--- src/main/java/com/emailmicroservice/core/util/DefaultResponse.java
+++ src/main/java/com/emailmicroservice/core/util/DefaultResponse.java
@@ -1,18 +1,24 @@
 package com.emailmicroservice.core.util;
 
+import lombok.Getter;
+
+@Getter
 public class DefaultResponse {
 
     public static final String ERROR = "Error";
+
     public static final String SUCESS = "Success";
 
     private final String status;
+
     private final String message;
 
     /**
      * This constructor will be deprecated soon. New factory approach will be soon implemented.
      * Please, consider using DefaultResponseImpl method 'asResponse()' for constant messages, or DefaultResponseFactory.create().
      */
-    public DefaultResponse(final String status, final String message) {
+    public DefaultResponse(final String status,
+                           final String message) {
         this.status = status;
         this.message = message;
     }
