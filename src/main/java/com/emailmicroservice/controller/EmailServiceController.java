9cf8f87d0aae0ac506e300aff934298c37ed7550
Felipe Babel <Felipe.Babel@philips.com>
2024-03-03 16:31:47 -0300

    fix: controller and swagger doc

--- src/main/java/com/emailmicroservice/controller/EmailServiceController.java
+++ src/main/java/com/emailmicroservice/controller/EmailServiceController.java
@@ -3,10 +3,12 @@ package com.emailmicroservice.controller;
 import com.emailmicroservice.application.EmailSenderService;
 import com.emailmicroservice.core.constant.Messages;
 import com.emailmicroservice.core.dto.EmailDto;
+import com.emailmicroservice.core.exception.EmailServiceException;
 import com.emailmicroservice.core.util.DefaultResponse;
 import com.emailmicroservice.infrastructure.amazonses.AmazonSesConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,15 +30,12 @@ public class EmailServiceController implements EmailService {
         this.amazonSesConfiguration = amazonSesConfiguration;
     }
 
-    @PostMapping
-    public ResponseEntity<DefaultResponse> sendEmail(@RequestBody EmailDto emailDto) {
-        try {
-            this.emailSenderService.sendEmail(emailDto);
-            return new ResponseEntity<>(new DefaultResponse(DefaultResponse.SUCESS, Messages.MSG_EMAIL_SENT_SUCCESSFUL), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(new DefaultResponse(DefaultResponse.ERROR, Messages.MSG_ERROR_WHEN_SEND_EMAIL), HttpStatus.BAD_REQUEST);
-        }
-
+    @PostMapping(
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DefaultResponse> sendEmail(@RequestBody EmailDto emailDto) throws EmailServiceException {
+        this.emailSenderService.sendEmail(emailDto);
+        return new ResponseEntity<>(new DefaultResponse(DefaultResponse.SUCESS, Messages.MSG_EMAIL_SENT_SUCCESSFUL), HttpStatus.OK);
     }
 
 }
