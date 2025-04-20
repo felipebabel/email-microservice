27837595c67840d66e8375eac1fd58f4218203e0
Felipe Babel <babelfelipe@gmail.com>
2025-04-20 11:28:38 -0300

    coverage

--- src/main/java/com/emailmicroservice/infrastructure/amazonses/AmazonSesConfiguration.java
+++ src/main/java/com/emailmicroservice/infrastructure/amazonses/AmazonSesConfiguration.java
@@ -28,7 +28,7 @@ public class AmazonSesConfiguration {
         return AmazonSimpleEmailServiceClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKeyId,
                         awsSecretKey)))
-                .withRegion(awsRegion).build(); // test
+                .withRegion(awsRegion).build();
     }
 
 }
